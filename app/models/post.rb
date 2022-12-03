5f1fe47f1a32cf846039305ed6ddcc82703af4ee
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-12-03 19:27:39 +0300

    Added validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,21 @@
 class Post < ApplicationRecord
   after_save :update_posts_counter
+  after_initialize :set_defaults
+
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :comments_counter, :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
-  def recent_five
-    comments.order(created_at: :desc).limit(5)
-  end
+@@ -10,6 +16,11 @@ def recent_five
 
   private
+
+  def set_defaults
+    self.comments_counter = 0
+    self.likes_counter = 0
+  end
 
   def update_posts_counter
     author.update(posts_counter: author.posts.all.length)
